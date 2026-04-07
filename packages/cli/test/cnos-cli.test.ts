6cf8cb0e401a317d1d4bc0addf03716eea9e209f
Prashant Sinha <7372627+pkvsinha@users.noreply.github.com>
2026-04-08 02:03:06 +0530

    releaes: 1.2.0

--- packages/cli/test/cnos-cli.test.ts
+++ packages/cli/test/cnos-cli.test.ts
@@ -241,7 +241,7 @@ describe('@kitsy/cnos-cli', () => {
   });
 
   it('prints the CLI version', () => {
-    expect(runVersion()).toBe('1.1.2');
+    expect(runVersion()).toBe('1.2.0');
   });
 
   it('shows current CLI context without creating .cnos-workspace.yml', async () => {
